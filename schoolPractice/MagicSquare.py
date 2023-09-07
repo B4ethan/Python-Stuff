a109c393e8da432df2f5af2123dc13c40be25c7b
Ethannn_boi <84193841+B4ethan@users.noreply.github.com>
2023-09-07 07:59:40 +0000

    added some stuff

--- schoolPractice/MagicSquare.py
+++ schoolPractice/MagicSquare.py
@@ -1,60 +1,34 @@
 import numpy as np
-import random
+from itertools import permutations
+from Square import Square
 
 class MagicSquare:
     def __init__(self, size):
+        self.MagicDict = { }
         self.size = size
-        self.magicSquare = random.sample(range(1, self.size**2+1), self.size**2)
-        self.magicSquare = np.reshape(self.magicSquare, (self.size, self.size))
+        self.combinations = []
 
-    def sumOfDiagonals(self):
-        #if the sum of the diagonals is euqals- return the sum, if not- return -1
-        sum = np.diag(self.magicSquare).sum()
-        if sum == np.diag(self.magicSquare[::-1])[::-1].sum():
-            return sum
-        else:
-            return -1
-    
-    def sumOfRows(self):
-        #if the sum of the lines is euqals- return the sum, if not- return -1
-        arrayOfSum = np.sum(self.magicSquare, axis = 0)
+        #creating an array of the numbers
+        for number in range(1, self.size**2+1):
+            self.combinations.append(number)
 
-        if np.all(arrayOfSum == arrayOfSum[0]): #cheking if all the rows has the same sum
-            return arrayOfSum[0]
-        else:
-            return -1
-    
-    def sumOfColumns(self):
-        #if the sum of the columns is equals - return the sum, if not - return -1
-        arrayOfSum = np.sum(self.magicSquare, axis = 1)
-
-        if np.all(arrayOfSum == arrayOfSum[0]):
-            return arrayOfSum[0]
-        else:
-            return -1
-   
-
-    def checkMagicSquare(self):
-        #if the square is magic- return true, else return false
-
-        if len(np.unique(self.magicSquare)) != len(self.magicSquare): #if there is at least one duplicate in the array
-            return False
-        
-        arraySums = np.array([self.sumOfDiagonals(), self.sumOfRows(), self.sumOfColumns()])
-
-        if -1 in arraySums: #if found -1
-            return False
-        
-        if np.all(arraySums == arraySums[0]):
-            return True
-        else:
-            return False
+        self.combinations = int(np.array(list(permutations(self.combinations))))
 
 
-#square = MagicSquare(3)
-#print(square.magicSquare)
-       
+    def addAllMagicToDict(self):
+        #checking all the array if they are magic square, if so, adding it to array
 
-ms = MagicSquare(3)
-print(ms.magicSquare)
-print(ms.checkMagicSquare())
+        valid = 0
+
+        for option in self.combinations:
+            square = Square(option)
+
+            if square.checkMagicSquare(): #if found magic square
+                self.MagicDict[valid] = np.arr_str(square.magicSquare)
+                valid = valid + 1
+
+
+s = MagicSquare(3)
+s.addAllMagicToDict()
+
+print(s.MagicDict)
---
+++ schoolPractice/Square.py
@@ -0,0 +1,55 @@
+import numpy as np
+import random
+import math
+
+class Square:
+    def __init__(self, digits):
+        self.digits = digits
+        self.magicSquare = np.reshape(self.digits, (math.sqrt(len(self.digits)), math.sqrt(len(self.digits))))
+
+
+    def sumOfDiagonals(self):
+        #if the sum of the diagonals is euqals- return the sum, if not- return -1
+        sum = np.diag(self.magicSquare).sum()
+        if sum == np.diag(self.magicSquare[::-1])[::-1].sum():
+            return sum
+        else:
+            return -1
+    
+    def sumOfRows(self):
+        #if the sum of the lines is euqals- return the sum, if not- return -1
+        arrayOfSum = np.sum(self.magicSquare, axis = 0)
+
+        if np.all(arrayOfSum == arrayOfSum[0]): #cheking if all the rows has the same sum
+            return arrayOfSum[0]
+        else:
+            return -1
+    
+    def sumOfColumns(self):
+        #if the sum of the columns is equals - return the sum, if not - return -1
+        arrayOfSum = np.sum(self.magicSquare, axis = 1)
+
+        if np.all(arrayOfSum == arrayOfSum[0]):
+            return arrayOfSum[0]
+        else:
+            return -1
+   
+
+    def checkMagicSquare(self):
+        #if the square is magic- return true, else return false
+
+        if len(np.unique(self.magicSquare)) != len(self.magicSquare): #if there is at least one duplicate in the array
+            return False
+        
+        arraySums = np.array([self.sumOfDiagonals(), self.sumOfRows(), self.sumOfColumns()])
+
+        if -1 in arraySums: #if found -1
+            return False
+        
+        if np.all(arraySums == arraySums[0]):
+            return True
+        else:
+            return False
+
+
+
