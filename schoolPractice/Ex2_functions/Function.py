67010b353c7405d000fcea8e717a64323b124b4e
B4ethan <eitan.lyakas@gmail.com>
2023-09-21 10:16:24 +0300

    didsomthing

--- schoolPractice/Ex2_functions/Function.py
+++ schoolPractice/Ex2_functions/Function.py
@@ -11,14 +11,17 @@ class Function:
         #return the function
         return f"{self.a}x^2 {self.b}x {self.c}"
     
+    def calcDerFunc(self):
+        self.derA = self.a * 2
+
     def findMinimum(self):
         #finding the minimum point
-        self.der = self.a * 2
+        self.calcDerFunc()
         numberToCheck = 0
 
         while abs(self.der) > self.EPSILON:
             numberToCheck -= self.der * self.EPSILON
-            self.der = self.a * 2 * numberToCheck + self.b
+            self.derA = self.a * 2 * numberToCheck + self.b
 
 
         return f"({numberToCheck}, {(self.a * numberToCheck**2) + self.b * numberToCheck + self.c})"
